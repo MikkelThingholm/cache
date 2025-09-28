8511348e79050d6285fd6a792fc5d19a3348a25a
MikkelThingholm <mikkelthingholm@gmail.com>
2025-09-28 21:54:36 +0200

    Implemented eviction of expired keys

--- lru.go
+++ lru.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
 type LRU[K comparable, V any] struct {
+	mu sync.Mutex
+
 	hashMap      map[K]*Node[K, V]
 	evictionList *LinkedList[K, V]
 	cap          int
+
+	defaultTTL      time.Duration
+	cleanupInterval time.Duration
+
+	stop chan struct{}
 }
 
 func NewLRU[K comparable, V any](capacity int) (*LRU[K, V], error) {
@@ -17,19 +26,43 @@ func NewLRU[K comparable, V any](capacity int) (*LRU[K, V], error) {
 
 	list := NewLinkedList[K, V]()
 	lru := &LRU[K, V]{
-		hashMap:      map[K]*Node[K, V]{},
-		evictionList: list,
-		cap:          capacity,
+		hashMap:         map[K]*Node[K, V]{},
+		evictionList:    list,
+		cap:             capacity,
+		defaultTTL:      time.Hour,
+		cleanupInterval: 5 * time.Minute,
+		stop:            make(chan struct{}),
+	}
+
+	if lru.cleanupInterval > 0 {
+		go func() {
+			ticker := time.NewTicker(lru.cleanupInterval)
+			defer ticker.Stop()
+			for {
+				select {
+				case <-ticker.C:
+					lru.removeExpired()
+				case <-lru.stop:
+					return
+				}
+			}
+		}()
 	}
 
 	return lru, nil
 }
 
-func (lru LRU[K, V]) Count() int {
+func (lru *LRU[K, V]) Stop() {
+	lru.stop <- struct{}{}
+}
+
+func (lru *LRU[K, V]) Count() int {
 	return lru.evictionList.Length()
 }
 
 func (lru *LRU[K, V]) Insert(key K, value V) {
+	lru.mu.Lock()
+	defer lru.mu.Unlock()
 
 	if node, ok := lru.get(key); ok {
 		lru.update(node, value)
@@ -39,12 +72,15 @@ func (lru *LRU[K, V]) Insert(key K, value V) {
 	lru.add(key, value)
 }
 
-func (lru *LRU[K, V]) Get(key K) (Node[K, V], error) {
+func (lru *LRU[K, V]) Get(key K) (Node[K, V], bool) {
+	lru.mu.Lock()
+	defer lru.mu.Unlock()
+
 	node, ok := lru.get(key)
 	if !ok {
-		return Node[K, V]{}, fmt.Errorf("key not found")
+		return Node[K, V]{}, false
 	}
-	return *node, nil
+	return *node, true
 }
 
 func (lru *LRU[K, V]) add(key K, value V) bool {
@@ -54,8 +90,9 @@ func (lru *LRU[K, V]) add(key K, value V) bool {
 	}
 
 	node := &Node[K, V]{
-		Key:   key,
-		Value: value,
+		Key:       key,
+		Value:     value,
+		ExpiresAt: time.Now().UnixMilli() + lru.defaultTTL.Milliseconds(),
 	}
 
 	if lru.evictionList.Length() == lru.cap {
@@ -84,5 +121,16 @@ func (lru *LRU[K, V]) get(key K) (*Node[K, V], bool) {
 
 func (lru *LRU[K, V]) update(node *Node[K, V], newValue V) {
 	node.Value = newValue
+	node.ExpiresAt = time.Now().UnixMilli() + lru.defaultTTL.Milliseconds()
 	lru.evictionList.MoveToHead(node)
+}
+
+func (lru *LRU[K, V]) removeExpired() {
+	lru.mu.Lock()
+	defer lru.mu.Unlock()
+
+	expiredNodes := lru.evictionList.FindExpiresBefore(time.Now().UnixMilli())
+	for i := range expiredNodes {
+		lru.remove(expiredNodes[i])
+	}
 }
--- lru_test.go
+++ lru_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"testing"
+	"testing/synctest"
+	"time"
 )
 
 func TestEviction(t *testing.T) {
@@ -14,26 +16,25 @@ func TestEviction(t *testing.T) {
 	lru.Insert(3, 3)
 	lru.Insert(4, 4)
 
-	_, err = lru.Get(1)
-	if err == nil {
-		t.Errorf("got nil, want error")
+	if _, ok := lru.Get(1); ok {
+		t.Errorf("expected key 1 to be envicted but was found")
 	}
 	if got, want := lru.Count(), 3; got != want {
 		t.Errorf("count: got %d, want %d", got, want)
 	}
 
-	node, err := lru.Get(2)
-	if err != nil {
-		t.Errorf("got %v, want nil", err)
+	node, ok := lru.Get(2)
+	if !ok {
+		t.Errorf("key 2 not found")
 	}
 	if want := 2; node.Key != want {
 		t.Errorf("node.Key: got %d, want %d", node.Key, want)
 	}
 
 	lru.Insert(5, 5)
-	_, err = lru.Get(3)
-	if err == nil {
-		t.Errorf("got nil, want error")
+	_, ok = lru.Get(3)
+	if ok {
+		t.Errorf("found key 3")
 	}
 	if got, want := lru.Count(), 3; got != want {
 		t.Errorf("count: got %d, want %d", got, want)
@@ -49,12 +50,57 @@ func Test_InsertingExistingKeyUpdatesValue(t *testing.T) {
 	lru.Insert(1, 1)
 	lru.Insert(1, 2)
 
-	node, err := lru.Get(1)
-	if err != nil {
-		t.Errorf("got %v, want nil", err)
+	node, ok := lru.Get(1)
+	if !ok {
+		t.Errorf("key 1 not found")
 	}
 	if got, want := node.Value, 2; got != want {
 		t.Errorf("got %d, want %d", got, want)
 	}
 
+}
+
+func Test_ExpiredItemsAreEvicted(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		lru, err := NewLRU[int, int](4)
+		defer lru.Stop()
+		if err != nil {
+			t.Fatalf("error while initializing LRU: %s", err)
+		}
+
+		lru.Insert(1, 1)
+		lru.Insert(2, 2)
+
+		time.Sleep(10 * time.Minute)
+
+		lru.Insert(3, 3)
+		lru.Insert(4, 4)
+		if lru.Count() != 4 {
+			t.Errorf("Expected lru count to be 4, got %d", lru.Count())
+		}
+
+		time.Sleep(50 * time.Minute)
+		synctest.Wait()
+
+		if lru.Count() != 2 {
+			t.Errorf("Expected lru count to be 2, got %d", lru.Count())
+		}
+		if _, found := lru.Get(1); found != false {
+			t.Errorf("Expected key 1 to be deleted")
+		}
+		if _, found := lru.Get(2); found != false {
+			t.Errorf("Expected key 2 to be deleted")
+		}
+		time.Sleep(10 * time.Minute)
+
+		if lru.Count() != 0 {
+			t.Errorf("Expected lru count to be 0, got %d", lru.Count())
+		}
+		if _, found := lru.Get(3); found != false {
+			t.Errorf("Expected key 1 to be deleted")
+		}
+		if _, found := lru.Get(4); found != false {
+			t.Errorf("Expected key 2 to be deleted")
+		}
+	})
 }
